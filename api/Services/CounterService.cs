b3a507779200e9779d46dfab840e6a149d16b708
John Russel Punio <johnrussel.punio@author-it.com>
2022-06-04 00:07:03 +0800

    add locking mechanism

--- api/Services/CounterService.cs
+++ api/Services/CounterService.cs
@@ -1,13 +1,13 @@
-using System.Collections.Generic;
-
 namespace api.Services;
 
 public class CounterService : ICounterService {
     private readonly Dictionary<string,int> _counter;
+    private readonly object _locker;
 
     public CounterService()
     {
         _counter = new Dictionary<string, int>();
+        _locker = new object();
     }
 
     public int Increment(string client = "Anonymous")
@@ -18,7 +18,11 @@ public class CounterService : ICounterService {
             return 1;
         }
 
-        _counter[client]++;
-        return _counter[client];
+        lock (_locker)
+        {
+            _counter[client]++;
+            Thread.Sleep(2000);
+            return _counter[client];
+        }
     }
 }
